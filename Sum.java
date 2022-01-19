f094bf7d770582fe52eeb1186cea413004c84284
Mohd Furkan <furkanchisti786@gmail.com>
2022-01-19 23:35:35 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Sum {
   public static void main(String[] args) {
     int a = 1;
     int b = 3;
-    System.out.println(a + b);
+    System.out.println("The sum is" + (a + b));
   }
 
 }
